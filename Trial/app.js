4c55375d49968b7837e88b421572cf0c43f965cb
BlackEagle12 <vicky.manavadariya321@gmail.com>
2024-07-13 00:35:06 +0530

    fix

--- Trial/app.js
+++ Trial/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 var cors = require("cors");
+const app = express();
 
 app.use(cors());
-const app = express();
 app.use(express.json());
 
 mongoose
